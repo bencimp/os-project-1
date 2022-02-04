97edb051417956f23e064a57c3b58603c665d255
Ben Cimperman <cimpe005@d.umn.edu>
2022-02-03 21:33:14 -0600

    added command line parsing
    currently prints out the archive name as to not create a compiler error

--- minitar_main.c
+++ minitar_main.c
@@ -12,9 +12,43 @@ int main(int argc, char **argv) {
 
     file_list_t files;
     file_list_init(&files);
-
     // TODO: Parse command-line arguments and invoke functions from 'minitar.h'
     // to execute archive operations
+
+    const char *operation = argv[1];
+    const char *archiveName = argv[3];
+    printf("%s\n", archiveName);
+    for (int x = 4; x < argc; x ++){
+      file_list_add(&files, argv[x]);
+    }
+    char create[] = "-c";
+    char append[] = "-a";
+    char list[] = "-l";
+    char update[] = "-u";
+    char extract[] = "-x";
+
+
+
+    if (strcmp(operation, create)){
+      //call create function
+    }
+    else if (strcmp(operation, append)){
+      //call append
+    }
+    else if (strcmp(operation, list)){
+      //call list
+    }
+    else if (strcmp(operation, update)){
+      //call upadte
+    }
+    else if (strcmp(operation, extract)){
+      //call extract
+    }
+    else {
+      //if not valid command, say so and do nothing
+      printf("Invalid command");
+    }
+                                      
 
     file_list_clear(&files);
     return 0;
